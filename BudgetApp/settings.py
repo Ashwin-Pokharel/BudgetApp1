a9aaf8c3d4b877d2c66400fa6ae479a8cc2be831
Kiran Pachhai <kiran.pachhai@gmail.com>
2019-12-24 15:33:47 -0500

    Updated gitignore and requirements

--- BudgetApp/settings.py
+++ BudgetApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['BudgetApp-dev.us-east-1.elasticbeanstalk.com','127.0.0.1',]
+ALLOWED_HOSTS = ['BudgetApp-dev.us-east-1.elasticbeanstalk.com','BudgetApp-final.us-east-1.elasticbeanstalk.com','127.0.0.1','localhost',]
 
 
 # Application definition
