929e66e3ffe4155f4c5331eabf33995c78b42a14
Ashwin_Pokharel <pokharel.ashwin1@gmail.com>
2019-12-25 20:20:03 -0500

    updated commands to python3 , changed assests location to make sure there is no conflict with collect static.

--- BudgetApp/settings.py
+++ BudgetApp/settings.py
@@ -139,8 +139,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 LOGIN_URL = '/home'
 LOGIN_REDIRECT_URL = "/login"
